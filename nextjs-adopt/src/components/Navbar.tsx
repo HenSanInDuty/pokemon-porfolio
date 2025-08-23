3c3a3d08c004c2ccf59a0a7a5caebda8a56eeb99
Hen <nhantt47@fpt.com>
2025-08-23 18:07:20 +0700

    fix: clear dark mode

--- nextjs-adopt/src/components/Navbar.tsx
+++ nextjs-adopt/src/components/Navbar.tsx
@@ -104,13 +104,6 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-4">
-          <button onClick={() => dispatch(switchMode())}>
-            <Image
-              src={isDarkMode ? assets.sun_icon : assets.moon_icon}
-              alt="moon"
-              className="w-6 cursor-pointer"
-            />
-          </button>
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo hover:bg-lightHover duration-500 dark:border-white/50 dark:hover:bg-darkHover"
